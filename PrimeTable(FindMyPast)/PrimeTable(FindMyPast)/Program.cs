3ef7e98a74a7c565d63edb2a7ce38504133dc6d8
cameronOBrien12 <cameronobrien12@hotmail.co.uk>
2018-01-17 15:22:01 +0000

    Added function to find if a number is prime
    There is now also titles for the X-Axis of all the prime numbers up to
    the users choice

--- PrimeTable(FindMyPast)/PrimeTable(FindMyPast)/Program.cs
+++ PrimeTable(FindMyPast)/PrimeTable(FindMyPast)/Program.cs
@@ -15,6 +15,11 @@ namespace PrimeTable_FindMyPast_
     {
         static void Main(string[] args)
         {
+            //Variables that we used to save whether a number is prime or not 
+            bool prime = false;
+            bool prime2 = false;
+
+
             //Get user input for size of table (table is of size "size+1")
             Console.WriteLine("Choose the size of your table");
             Console.WriteLine("Enter choice: ");
@@ -22,6 +27,52 @@ namespace PrimeTable_FindMyPast_
             Console.WriteLine("Your choice is: " + size);
             //Start of table output
             Console.Write("*   | ");
+
+            //Finds the Prime numbers used for the X-Axis
+            for (int i = 1; i < size + 2; i++)
+            {
+                //Uses my isprime function to check each number
+                prime = IsPrime(i);
+                if (prime == true)
+                {
+                    //if statement needed for formatting of table with different digit length numbers
+                    if (i < 10)
+                        Console.Write(i + "   | ");
+                    else if (i > 9 && i < 100)
+                        Console.Write(i + "  | ");
+                    else if (i > 99 && i < 1000)
+                        Console.Write(i + " | ");
+                    else
+                        Console.Write(i + "| ");
+                }
+            }
+
+        }
+
+        //method used to check if a number is prime
+        static bool IsPrime(int i)
+        {
+            //1 is not prime
+            if (i == 1)
+            {
+                return false;
+            }
+            //check all nubmers below current
+            for (int j = 2; j <= i; j++)
+            {
+                // but dont check divide by itself
+                if (j != i)
+                {
+                    //if divides by any other number
+                    if (i % j == 0)
+                    {
+                        //not prime
+                        return false;
+                    }
+                }
+            }
+            //else it is prime
+            return true;
         }
     }
 }
