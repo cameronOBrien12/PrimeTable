2756d4ce51d10fe4c5808123749f93c824001138
cameronOBrien12 <cameronobrien12@hotmail.co.uk>
2018-01-17 16:25:00 +0000

    Final Updates and such

--- PrimeTable(FindMyPast)/PrimeTable(FindMyPast)/Program.cs
+++ PrimeTable(FindMyPast)/PrimeTable(FindMyPast)/Program.cs
@@ -46,7 +46,7 @@ namespace PrimeTable_FindMyPast_
                         Console.Write(i + "| ");
                 }
             }
-            
+
             //formatting table
             Console.WriteLine("");
             Console.WriteLine("----+---------");
@@ -96,8 +96,21 @@ namespace PrimeTable_FindMyPast_
             }
             //statement to exit program
             Console.WriteLine("");
-            Console.Write("Press enter to exit");
+            Console.WriteLine("Paused - Press enter to see test results for isPrime function");
             string exit = Console.ReadLine();
+            Console.WriteLine("TEST RESULTS:");
+            PrimeTesting();
+            Console.WriteLine("");
+            Console.WriteLine("");
+            
+            Console.WriteLine("GENERATOR OF PRIME NUMBERS");
+            Console.WriteLine("This is set to print the first million primes, you may want to end it early :)");
+            Console.WriteLine("Paused - Press enter to continue to generator ( CTRL-C to escape early )");
+            exit = Console.ReadLine();
+            PrimeNumberGenerator();
+
+            Console.Write("Press enter to exit");
+             exit = Console.ReadLine();
         }
 
         //Method which finds if a number is prime or not, takes an int, returns a boolean
@@ -124,6 +137,48 @@ namespace PrimeTable_FindMyPast_
             }
             //else it is prime
             return true;
+        }
+
+        //some testing for the isPrime method
+        static void PrimeTesting()
+        {
+           Console.WriteLine("Is 3 prime? " + IsPrime(3) );
+           Console.WriteLine("Is 7 prime? " + IsPrime(7) );
+           Console.WriteLine("Is 1,000,000 prime? " + IsPrime(1000000));
+           Console.WriteLine("Next one takes a few seconds...");
+           Console.WriteLine("Is 533,000,389 prime? " + IsPrime(533000389));
+
+        }
+
+        //I realised I didn't really use a prime number generator in my program, 
+        //so I am adding this method to show that I know how to create one 
+        static void PrimeNumberGenerator()
+        {
+            //counts which prime number we are on
+            int PrimeCount = 0;
+            //starting at first prime number
+            int number = 2;
+            //loop until at prime number
+            do
+            {
+                //if number is prime
+                if (IsPrime(number))
+                {
+                    //add to prime count
+                    PrimeCount++;
+                    Console.WriteLine(number);
+                }
+
+                //check next number
+                number++;
+
+                //CHANGE THIS VARIABLE TO GET ALL PRIMES UP TO HOW MANY YOU NEED, e.g. first million
+            } while (PrimeCount != 1000000);
+
+            //return to previous number - 1000000st prime number
+            number--;
+            //write to console
+            Console.WriteLine("The 1000000 Prime Number is: {0}", number);
         }
     }
 }
--- README.md
+++ README.md
@@ -14,3 +14,12 @@ Visual Studio: In Explorer tab open "Program.cs" to see source code
 Press "Start" to run program
 
 NOTE: Program works for large inputs, although formatting messes up once it hits screen length.
+
+I'm pleased with the logic behind the program. It all runs smoothly and in a short time. 
+If I was to spend more time on this project I would create a GUI and add in much more input validation/security to the program.
+
+NOTE: I have realised I have not used a traditional prime number generator for the table, I have added one in after to show that I know how to do it.
+
+Thanks for considering my application.
+
+Cameron. 
